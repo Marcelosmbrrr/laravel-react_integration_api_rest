99a5ad7fbd24636c75fd7af2156573100f92708e
Marcelo Moreira <profissionalsmbr@gmail.com>
2022-06-03 21:15:46 -0300

    backend att

---
+++ backend/app/Models/Telephones/TelephoneModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Telephones;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TelephoneModel extends Model
+{
+    use HasFactory;
+}
